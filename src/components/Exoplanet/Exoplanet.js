5dba2c407b886f4bd5a802953616725e58b3da72
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-14 23:19:08 +0400

    sphere is smoother

--- src/components/Exoplanet/Exoplanet.js
+++ src/components/Exoplanet/Exoplanet.js
@@ -3,8 +3,8 @@ import { OrbitControls } from "@react-three/drei";
 const Exoplanet = () => {
   return (
     <mesh>
-      <OrbitControls />
-      <sphereGeometry />
+      <OrbitControls minDistance={3} maxDistance={6} dampingFactor={0.1} />
+      <sphereGeometry args={[1, 128, 128]} />
     </mesh>
   );
 };
