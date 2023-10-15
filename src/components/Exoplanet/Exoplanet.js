6778ea4206d82adddcd952940c7e9b8db4fb1581
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-15 12:55:18 +0400

    exoplanet added

--- src/components/Exoplanet/Exoplanet.js
+++ src/components/Exoplanet/Exoplanet.js
@@ -1,10 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
 
+import { TextureLoader } from "three";
+
+import ExoplanetTexture from "../../assets/exoplanet.png";
+
 const Exoplanet = () => {
+  const texture = new TextureLoader().load(ExoplanetTexture);
+
   return (
     <mesh>
       <OrbitControls minDistance={3} maxDistance={6} dampingFactor={0.1} />
       <sphereGeometry args={[1, 128, 128]} />
+      <meshBasicMaterial map={texture} />
     </mesh>
   );
 };
