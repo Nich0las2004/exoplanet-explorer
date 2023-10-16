1ce965f505fb5a9313ae2407246d02f3b2d47c29
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-16 09:49:27 +0400

    website detect when a planet is clicked

--- src/components/Exoplanet/Exoplanet.js
+++ src/components/Exoplanet/Exoplanet.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { OrbitControls } from "@react-three/drei";
 
 import { TextureLoader } from "three";
@@ -5,10 +6,18 @@ import { TextureLoader } from "three";
 import ExoplanetTexture from "../../assets/exoplanet.png";
 
 const Exoplanet = () => {
+  const [planetClicked, setPlanetClicked] = useState(false);
+
   const texture = new TextureLoader().load(ExoplanetTexture);
 
+  const planetClickHandler = () => {
+    setPlanetClicked(() => !planetClicked);
+
+    alert("planet clicked");
+  };
+
   return (
-    <mesh>
+    <mesh onClick={planetClickHandler}>
       <OrbitControls minDistance={3} maxDistance={6} dampingFactor={0.1} />
       <sphereGeometry args={[1, 128, 128]} />
       <meshBasicMaterial map={texture} />
