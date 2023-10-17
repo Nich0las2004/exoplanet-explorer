99fe92629cab9a25bd7c1ea2cea5cdfd138cb0dc
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-17 19:17:56 +0400

    the planet rotates

--- src/components/Exoplanet/Exoplanet.js
+++ src/components/Exoplanet/Exoplanet.js
@@ -1,24 +1,42 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { OrbitControls } from "@react-three/drei";
 
 import { TextureLoader } from "three";
 
 import ExoplanetTexture from "../../assets/exoplanet.png";
+import { useFrame } from "@react-three/fiber";
 
 const Exoplanet = () => {
+  const planetRef = useRef();
+
   const [planetClicked, setPlanetClicked] = useState(false);
+
+  const controls = useRef();
 
   const texture = new TextureLoader().load(ExoplanetTexture);
 
   const planetClickHandler = () => {
-    setPlanetClicked(() => !planetClicked);
+    setPlanetClicked(true);
 
-    alert("planet clicked");
+    alert("planet clicked!");
   };
 
+  useFrame(() => {
+    if (planetRef.current) {
+      planetRef.current.rotation.z += 0.001;
+    }
+  });
+
   return (
-    <mesh onClick={planetClickHandler}>
-      <OrbitControls minDistance={3} maxDistance={6} dampingFactor={0.1} />
+    <mesh ref={planetRef} onClick={planetClickHandler}>
+      {planetClicked === false && (
+        <OrbitControls
+          ref={controls}
+          minDistance={3}
+          maxDistance={6}
+          dampingFactor={0.1}
+        />
+      )}
       <sphereGeometry args={[1, 128, 128]} />
       <meshBasicMaterial map={texture} />
     </mesh>
